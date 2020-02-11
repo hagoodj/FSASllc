559939aaf7fe2f058b56e4cb46554186453ffd70
jordan hagood <jordan.hagood@yahoo.com>
2020-02-11 10:51:21 -0800

    spacing & grammar changes

---
+++ public/js/fsas.js
@@ -0,0 +1,22 @@
+$( window ).on( "load", function() {
+
+    // var documentEl = $(document);
+    // var fadeElement = $('.container');
+
+    // fadeElement.each(function () {
+    //     var $this = $(this);
+    //     $this.delay(2000).fadeIn('slow');
+    // })
+
+    // documentEl.on('scroll', function() {
+    //     var scrollPos = documentEl.scrollTop();
+
+    //     fadeElement.each(function() {
+    //         var $this = $(this);
+    //         var elementOffsetTop = $this.offset().top;
+    //         if(scrollPos > elementOffsetTop) $this.css('opacity', 1 - (scrollPos-elementOffsetTop)/400);
+    //     })
+
+    // })
+
+});
