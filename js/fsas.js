ec369b2d643ede9ea26cdf6414df70bee4248c8b
Hagood <Jordan-Hagood@ouhsc.edu>
2020-12-11 16:33:18 -0600

    animation debug - fade in elements when top reaches middle of window

--- js/fsas.js
+++ js/fsas.js
@@ -5,12 +5,19 @@ $(window).on("load", function () {
 
     function isScrolledIntoView($elem, $window) {
         var docViewTop = $window.scrollTop();
+        console.log("docViewTop: " + docViewTop);
         var docViewBottom = docViewTop + $window.height();
+        console.log("docViewBottom: " + docViewBottom);
 
         var elemTop = $elem.offset().top;
+        console.log("elementTop: " + elemTop);
         var elemBottom = elemTop + $elem.height();
+        console.log("elementBottom: " + elemBottom);
 
-        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        return(elemTop <= (docViewTop + docViewBottom)/2)
+
+        //return ((elemBottom <= docViewBottom + $elem.height()) &&(elemTop >= docViewTop));
+       
     }
     $(document).on("scroll", function () {
         $elem.each(function() {
